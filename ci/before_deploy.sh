57a8e02965d02decea304e62d02651f994c08ae9
sigoden <sigoden@gmail.com>
2019-04-01 15:22:28 +0800

    fix: ci/before_deploy

--- ci/before_deploy.sh
+++ ci/before_deploy.sh
@@ -18,7 +18,7 @@ main() {
     test -f Cargo.lock || cargo generate-lockfile
 
     # TODO Update this to build the artifacts that matter to you
-    cross rustc --bin upt --target $TARGET --release -- -C lto
+    cargo rustc --bin upt --target $TARGET --release -- -C lto
 
     # TODO Update this to package the right artifacts
     cp target/$TARGET/release/upt $stage/
