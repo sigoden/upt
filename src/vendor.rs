80c038fb1c8949b081f43f8656b3839b7bfec597
Montegasppα Cacilhας <montegasppa@cacilhas.info>
2024-08-08 08:27:12 -0300

    yay can be used for info too

--- src/vendor.rs
+++ src/vendor.rs
@@ -252,7 +252,7 @@ vendors![
         remove: "pacman -R -s $",
         upgrade: "yay --cleanafter -S $",
         search: "yay -S -s $",
-        info: "pacman -S -i $",
+        info: "yay -S -i $",
         update_index: "yay -S -y",
         upgrade_all: "yay -S -y -u",
         list_installed: "pacman -Q",
